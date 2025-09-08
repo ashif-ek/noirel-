ab0aba3a3fff65cc0e3df6fcf78ec1e558cf5235
Ashif EK <ashifek11@gmail.com>
2025-09-08 15:13:14 +0530

    serverr

--- my-app/src/auth/api.jsx
+++ my-app/src/auth/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const Api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://noirel-server.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
