6f97869dbcb0da185e1e0efaaff0ff1411914e01
Ashif EK <ashifek11@gmail.com>
2025-09-09 23:04:06 +0530

    api

--- my-app/src/modules/admin/adminLayout.jsx
+++ my-app/src/modules/admin/adminLayout.jsx
@@ -38,12 +38,15 @@
 
 
 
-
 import { Link, Outlet, useLocation } from "react-router-dom";
-import { FiHome, FiBox, FiUsers } from "react-icons/fi";
+import { FiHome, FiBox, FiUsers, FiLogOut } from "react-icons/fi";
+import { useAuth } from "../../context/AuthContext";
+
 
 export default function AdminLayout() {
   const location = useLocation();
+    const { logout } = useAuth();
+  
 
   const navItems = [
     { name: "Home", path: "/admin", icon: <FiHome /> },
@@ -51,30 +54,45 @@ export default function AdminLayout() {
     { name: "Users", path: "/admin/users", icon: <FiUsers /> },
   ];
 
+
+
+  
+
   return (
     <div className="flex min-h-screen bg-gray-100">
       {/* Sidebar */}
-      <aside className="w-64 bg-white border-r shadow-sm">
-        <div className="p-6 border-b">
-          <h2 className="text-2xl font-bold text-gray-800">Admin</h2>
+      <aside className="w-64 bg-white border-r shadow-sm flex flex-col justify-between">
+        <div>
+          <div className="p-6 border-b">
+            <h2 className="text-2xl font-bold text-gray-800">Admin</h2>
+          </div>
+
+          <nav className="mt-4 space-y-1">
+            {navItems.map((item) => (
+              <Link
+                key={item.path}
+                to={item.path}
+                className={`flex items-center gap-3 px-4 py-2 text-gray-700 rounded-lg transition-colors ${
+                  location.pathname === item.path
+                    ? "bg-blue-50 text-blue-600 font-medium"
+                    : "hover:bg-gray-100"
+                }`}
+              >
+                {item.icon}
+                <span>{item.name}</span>
+              </Link>
+            ))}
+          </nav>
         </div>
 
-        <nav className="mt-4 space-y-1">
-          {navItems.map((item) => (
-            <Link
-              key={item.path}
-              to={item.path}
-              className={`flex items-center gap-3 px-4 py-2 text-gray-700 rounded-lg transition-colors ${
-                location.pathname === item.path
-                  ? "bg-blue-50 text-blue-600 font-medium"
-                  : "hover:bg-gray-100"
-              }`}
-            >
-              {item.icon}
-              <span>{item.name}</span>
-            </Link>
-          ))}
-        </nav>
+        {/* Logout Button */}
+        <button
+          onClick={logout}
+          className="flex items-center gap-3 px-4 py-2 text-gray-700 rounded-lg mb-4 ml-4 hover:bg-red-100 hover:text-red-600 transition-colors"
+        >
+          <FiLogOut />
+          <span>Logout</span>
+        </button>
       </aside>
 
       {/* Main content */}
